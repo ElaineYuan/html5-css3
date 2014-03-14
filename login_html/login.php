ec21c44b917b665c7df5fa597bd68443c072f117
ElaineYuan <yuanpianpian05@126.com>
2014-03-15 00:15:18 +0800

    php中echo中是可以用逗号或者点号来连接字符串的.并且经过测试.这样的连接字符串方式要比直接用点号要快.

--- login_html/login.php
+++ login_html/login.php
@@ -3,14 +3,14 @@
 $username = $_POST['login'];
 $password = $_POST['password'];
 
-echo htmlspecialchars($_POST['login'])'输入的用户名<br />';
-echo MD5($_POST['password'])'输入的密码加密后：<br />';
+echo '输入的用户名：',htmlspecialchars($_POST['login']),'<br />';
+echo '输入的密码加密后：',MD5($_POST['password']),'<br />';
 
 //检测用户名及密码是否正确
 if($username=='pianpian'&&$password=='ypp'){
     
     echo $username,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
-	echo $password'<br />';
+	echo $password,'<br />';
     echo '点击此处 <a href="login.php?action=logout">注销</a> 登录！<br />';
     exit;
 } else {
