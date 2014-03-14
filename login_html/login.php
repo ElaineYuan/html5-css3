1b09657c5726f02cb47cc6ce3c0027beeae6c0e2
ElaineYuan <yuanpianpian05@126.com>
2014-03-14 23:53:48 +0800

    ..

--- login_html/login.php
+++ login_html/login.php
@@ -3,15 +3,14 @@
 $username = $_POST['login'];
 $password = $_POST['password'];
 
-echo '输入的用户名：'htmlspecialchars($_POST['login'])<br />;
-echo '输入的密码加密后：'MD5($password)<br />;
+echo htmlspecialchars($_POST['login'])'输入的用户名<br />';
+echo MD5($password)'输入的密码加密后：<br />';
 
 //检测用户名及密码是否正确
 if($username=='pianpian'&&$password=='ypp'){
-
     
     echo $username,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
-	echo $password<br />;
+	echo $password'<br />';
     echo '点击此处 <a href="login.php?action=logout">注销</a> 登录！<br />';
     exit;
 } else {
