78c2a6c4e520f719d815d26b3eb56ddd5523a3c0
ElaineYuan <yuanpianpian05@126.com>
2014-03-26 10:47:46 +0800

    hello world

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
  </head>
  <body>
- 	<?
- 	php echo  "<p>Hello World</p>";
+ 	<?php
+	   echo  "<p>Hello World</p>";
  	 ?> 
  </body>
 </html>
